5d3d768efe668af17c44e9b8b6b119a23f6b30db
Breno Oliveira <breno6@ba.estudante.senai.br>
2024-07-08 19:43:54 -0300

    Add RouteGroup

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-})->middleware('auth');;
-Route::get('/cadastro/alunos', [AlunoController::class, 'index'])->middleware('auth');
-Route::get('/buscar/alunos{id?}', [AlunoController::class, 'read'])->middleware('auth');
-Route::put('/buscar/alunos', [AlunoController::class, 'update'])->middleware('auth');
-Route::delete('/delete/aluno', [AlunoController::class, 'delete'])->middleware('auth');
-Route::post('/cadastro/alunos', [AlunoController::class, 'store'])->middleware('auth');
+})->middleware('auth');
+Route::group(['middleware' => ['auth']], function(){
+    Route::get('/cadastro/alunos', [AlunoController::class, 'index']);
+    Route::post('/cadastro/alunos', [AlunoController::class, 'store']);
+    Route::get('/buscar/alunos{id?}', [AlunoController::class, 'read']);
+    Route::put('/buscar/alunos', [AlunoController::class, 'update']);
+    Route::delete('/delete/aluno', [AlunoController::class, 'delete']);
+});
 
-Route::get('/treino', [TreinoController::class, 'show'])->middleware('auth');
-Route::post('/treino', [TreinoController::class, 'store'])->middleware('auth');
+Route::group(['middleware' => ['auth']], function(){
+    Route::get('/treino', [TreinoController::class, 'show'])->middleware('auth');
+    Route::post('/treino', [TreinoController::class, 'store'])->middleware('auth');
+});
 
-Route::get('/consultar', function () {return view('treino.consultarTreino');});
-Route::post('/consultar', [TreinoController::class, 'consultarTreino']);
+
+Route::group([],function (){
+    Route::get('/consultar', function () {return view('treino.consultarTreino');});
+    Route::post('/consultar', [TreinoController::class, 'consultarTreino']);
+});
+
 
 Route::get('/treino/imprimir/{aluno}/{treino}', [TreinoController::class, 'imprimirTreino']);
 
@@ -26,6 +34,7 @@ Route::get('/treino/imprimir/{aluno}/{treino}', [TreinoController::class, 'impri
 Route::middleware([ 
     'auth:sanctum',
     config('jetstream.auth_session'),
+
     'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
